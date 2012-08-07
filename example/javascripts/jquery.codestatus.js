c415140e4f489b196ea0c5da6aeee9077a4b6cf7
Joseph Silvashy <jpsilvashy@gmail.com>
2012-08-06 18:01:55 -0700

    added a lot of JS

--- example/javascripts/jquery.codestatus.js
+++ example/javascripts/jquery.codestatus.js
@@ -15,7 +15,7 @@
     // array of all supported providers
     var availableProviders = {
       travisci: 'http://travis-ci.org/',
-      gemnasium: 'https://gemnasium.com/'
+      // gemnasium: 'https://gemnasium.com/'
     };
 
     // selected providers
@@ -25,7 +25,7 @@
     return this.each(function() {
 
       // change state
-      $(this).html("loading...");
+      // $(this).html("loading...");
 
       // make json requests to each provider
       console.log(settings.providers);
@@ -36,17 +36,24 @@
         var url = baseUrl + options.repository
         console.log('make request to: ', url)
 
-        // make requests
-        // $.ajax({
-        //   url: url,
-        //   dataType: 'json',
-        //   data: data,
-        //   success: console.log('success', data)
-        // });
-
-        $.getJSON(url + '.json', function(data) {
-          console.log('data: ', data)
+        // make request
+        $.ajax({
+          url: url + '.json',
+          dataType: 'jsonp',
+          success: function(data) {
+            if (data.last_build_result == 0) {
+              console.log('Passed');
+            } else {
+              console.log('Failed');
+            }
+          }
         });
+
+        // build html object and insert it
+        // this should be in the scope of the *this* from the outer function
+        // that way we can hook on to the same html object that called the
+        // function, this is hacked right now.
+        $('#codestatus').append('append')
 
       });
 
